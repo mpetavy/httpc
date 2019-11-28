614249d7cd4fed67deefb8a30d3754854a0a0ea9
Marcel Petavy <marcel.petavy@zeiss.com>
2019-11-28 13:34:22 +0100

    httpc with ignore tls, restartConfiguration/deleteConfiguration with app restart

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"crypto/tls"
 	"encoding/base64"
 	"flag"
 	"fmt"
@@ -15,23 +16,25 @@ import (
 )
 
 var (
-	filename *string
-	url      *string
-	username *string
-	password *string
-	method   *string
-	header   *string
+	filename  *string
+	url       *string
+	username  *string
+	password  *string
+	method    *string
+	header    *string
+	ignoreTls *bool
 )
 
 func init() {
 	common.Init("1.0.4", "2017", "simple http tool", "mpetavy", common.APACHE, false, nil, nil, run, 0)
 
-	url = flag.String("u", "", "URL to JNLP file")
+	url = flag.String("c", "", "connection URL")
 	filename = flag.String("f", "", "filename")
-	username = flag.String("username", "", "username")
-	password = flag.String("password", "", "password")
-	method = flag.String("method", "GET", "http method")
-	header = flag.String("header", "", "header")
+	username = flag.String("u", "", "username")
+	password = flag.String("p", "", "password")
+	method = flag.String("m", "GET", "http method")
+	header = flag.String("h", "", "header")
+	ignoreTls = flag.Bool("i", true, "header")
 }
 
 func basicAuth(username, password string) string {
@@ -40,12 +43,19 @@ func basicAuth(username, password string) string {
 }
 
 // download loads a remote resource via http(s) and stores it to the given filename
-func download() (*bytes.Buffer, error) {
+func download() (*http.Response, *bytes.Buffer, error) {
 	var b bytes.Buffer
+
+	if *ignoreTls {
+		http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	}
 
 	client := &http.Client{}
 
 	request, err := http.NewRequest(*method, *url, nil)
+	if common.Error(err) {
+		return nil, nil, err
+	}
 
 	if *username != "" {
 		request.Header.Add("Authorization", "Basic "+basicAuth(*username, *password))
@@ -58,7 +68,7 @@ func download() (*bytes.Buffer, error) {
 			kv := strings.Split(h, "=")
 
 			if len(kv) < 2 {
-				return nil, fmt.Errorf("invalid header format: %s", h)
+				return nil, nil, fmt.Errorf("invalid header format: %s", h)
 			}
 
 			request.Header.Add(kv[0], kv[1])
@@ -68,49 +78,48 @@ func download() (*bytes.Buffer, error) {
 	// get a response from the remote source
 	response, err := client.Do(request)
 	// response, err := http.Get(href)
-	if err != nil {
-		return &b, err
+	if common.Error(err) {
+		return nil, &b, err
 	}
 
 	// care about final cleanup of reponse body
-	defer response.Body.Close()
+	defer func() {
+		common.Error(response.Body.Close())
+	}()
 
 	// download the remote resource to the file
 	_, err = io.Copy(&b, response.Body)
-	if err != nil {
-		return nil, err
+	if common.Error(err) {
+		return nil, nil, err
 	}
 
-	return &b, nil
+	return response, &b, nil
 }
 
 func run() error {
 	now := time.Now()
 
-	b, err := download()
+	r, b, err := download()
 
-	if err != nil {
+	if common.Error(err) {
 		return err
 	}
 
-	if err == nil {
-		elapsed := int64(time.Since(now).Nanoseconds()) / 1000 / 1000
-		fmt.Printf("time needed: %d msec\n", elapsed)
-		fmt.Printf("bytes written: %d\n", b.Len())
+	elapsed := int64(time.Since(now).Nanoseconds()) / 1000 / 1000
+	fmt.Printf("time needed:   %d msec\n", elapsed)
+	fmt.Printf("status:        %s\n", r.Status)
+	fmt.Printf("bytes written: %d\n", b.Len())
+	fmt.Printf("body:\n")
 
-		if *filename != "" {
-			err = ioutil.WriteFile(*filename, b.Bytes(), common.DefaultFileMode)
-			if err != nil {
-				if err != nil {
-					return err
-				}
-			}
-
-			fmt.Printf("bytes written to file: %s\n", *filename)
-		} else {
-			fmt.Printf("%s\n", string(b.Bytes()))
+	if *filename != "" {
+		err = ioutil.WriteFile(*filename, b.Bytes(), common.DefaultFileMode)
+		if common.Error(err) {
+			return err
 		}
 
+		fmt.Printf("bytes written to file: %s\n", *filename)
+	} else {
+		fmt.Printf("%s\n", string(b.Bytes()))
 	}
 
 	return nil
@@ -119,5 +128,5 @@ func run() error {
 func main() {
 	defer common.Done()
 
-	common.Run([]string{"u"})
+	common.Run([]string{"c"})
 }
